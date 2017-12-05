e76e9eaf91cc354841f01f181df072830ff2fa1c
Thomas Bach <hacking@babibo.de>
2017-12-05 13:05:28 +0100

    cassandra: upgrade to 3.11
    Remove older versions 2.1, 2.2 and 3.0. The 3.11 branch is set as
    default.

--- pkgs/servers/nosql/cassandra/3.0.nix
+++ pkgs/servers/nosql/cassandra/3.0.nix
@@ -1,6 +1,6 @@
 { callPackage, ... } @ args:
 
 callPackage ./generic.nix (args // {
-  version = "3.0.9";
-  sha256 = "16jdh20cr4h47ldjqlnp2cdnb9zshqvnll6995s2a75d8m030c0g";
+  version = "3.0.15";
+  sha256 = "af340e40987abad15b600438acea2521586bab4f5c72f914c981d457eee522d9";
 })
--- pkgs/servers/nosql/cassandra/3.11.nix
+++ pkgs/servers/nosql/cassandra/3.11.nix
@@ -2,5 +2,5 @@
 
 callPackage ./generic.nix (args // {
   version = "3.11.1";
-  sha256 = "1vgh4ysnl4xg8g5v6zm78h3sq308r7s17ppbw0ck4bwyfnbddvkg";
+  sha256 = "6feed696759e2f3219e0ebde13f4c9080cac0744a77eb3cb43af136ab527f0ed";
 })
