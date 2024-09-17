c744ee1fa13664b684c3709a6e16096f29c6408f
Robert Hensing <robert@roberthensing.nl>
2024-09-17 15:01:35 +0200

    nixosTests.nix-serve: Use new entrypoint
    This reuses the `pkgs`, so that `pkgs.nixosTests.nix-serve` will
    run with the overlays of `pkgs` applied.

--- nixos/tests/nix-serve.nix
+++ nixos/tests/nix-serve.nix
@@ -1,4 +1,4 @@
-import ./make-test-python.nix ({ pkgs, ... }:
+{ config, ... }:
 {
   name = "nix-serve";
   nodes.machine = { pkgs, ... }: {
@@ -6,10 +6,11 @@ import ./make-test-python.nix ({ pkgs, ... }:
     environment.systemPackages = [
       pkgs.hello
     ];
+    # virtualisation.diskSize = 10 * 1024;
   };
   testScript = let
     pkgHash = builtins.head (
-      builtins.match "${builtins.storeDir}/([^-]+).+" (toString pkgs.hello)
+      builtins.match "${builtins.storeDir}/([^-]+).+" (toString config.node.pkgs.hello)
     );
   in ''
     start_all()
@@ -19,4 +20,4 @@ import ./make-test-python.nix ({ pkgs, ... }:
         "curl --fail -g http://0.0.0.0:5000/nar/${pkgHash}.nar -o /tmp/hello.nar"
     )
   '';
-})
+}
